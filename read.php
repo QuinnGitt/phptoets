7d53666e0004817c37854a8b62bcdd222d75fab5
Quinty <quintydewinter1@gmail.com>
2023-01-13 12:30:48 +0100

    Klaar filmpje gemaakt

--- read.php
+++ read.php
@@ -54,7 +54,7 @@ foreach ($result as $info) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RichestPeople</title>
+    <title>Dure Auto</title>
 </head>
 <body>
     <h3>De vijf duurste auto's ter wereld</h3>
